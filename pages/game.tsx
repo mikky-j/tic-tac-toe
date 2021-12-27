cb3abb57a79f86a9a8c65aa7714dda3cfac2ce19
kvng_mikky <moregunwa@gmail.com>
2021-12-27 16:47:12 +0100

    added the multiple game modes feature

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,18 +1,19 @@
 import { Button } from "@chakra-ui/button";
 import { Box, VStack, Text, Heading, Flex, Center } from "@chakra-ui/layout";
+import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/table";
 import type { NextPage } from "next";
 import { NextRouter, withRouter } from "next/router";
-import { ParsedUrlQuery } from "querystring";
 import React, { FC, useState } from "react";
 
 type GameMode = "single" | "b-3" | "b-5" | "b-10";
 type GameType = "X" | "O" | null;
+type GameScore = 1 | 0 | "-";
 const squares: Array<GameType> = Array(9).fill(null);
 interface RouterProps {
   router: NextRouter;
 }
 interface BoardProps {
-  mode: string | string[] | undefined;
+  mode: GameMode;
 }
 interface SquareProps {
   index: number;
@@ -43,11 +44,31 @@ const Square: FC<SquareProps> = (props) => {
     </Flex>
   );
 };
-const Board: FC<BoardProps> = () => {
-  let [winner, setWinner] = useState<GameType>(null);
+const Board: FC<BoardProps> = ({ mode }) => {
+  const [winner, setWinner] = useState<GameType>(null);
+  var numberOfGames = 1;
+  const [currentGame, setCurrentGame] = useState(0);
+  const [isDraw, setDraw] = useState(false);
+  const isSingle = mode === "single";
+  if (!isSingle) {
+    switch (mode) {
+      case "b-3":
+        numberOfGames = 3;
+        break;
+      case "b-5":
+        numberOfGames = 5;
+        break;
+      case "b-10":
+        numberOfGames = 10;
+        break;
+    }
+  }
+  const [scores, updateScore] = useState(
+    Array<GameScore[]>(numberOfGames).fill(["-", "-"])
+  );
   const [isX, changeX] = useState(true);
   const handleClick = (index: number): void => {
-    if (squares[index] === null && winner === null) {
+    if (squares[index] === null && winner === null && !isDraw) {
       squares[index] = isX ? "X" : "O";
       changeX(!isX);
     }
@@ -62,15 +83,35 @@ const Board: FC<BoardProps> = () => {
       [2, 5, 8],
       [0, 4, 8],
       [2, 4, 6],
+      [3, 4, 5],
     ];
     for (let index = 0; index < winningLines.length; index++) {
       const [a, b, c] = winningLines[index];
       if (
         squares[a] === squares[b] &&
         squares[b] === squares[c] &&
-        squares[a] !== null
+        squares[a] !== null &&
+        winner === null
       ) {
+        if (squares[a] === "X") {
+          if (currentGame % 2 === 0) {
+            scores[currentGame] = [1, 0];
+          } else {
+            scores[currentGame] = [0, 1];
+          }
+        } else {
+          if (currentGame % 2 === 0) {
+            scores[currentGame] = [0, 1];
+          } else {
+            scores[currentGame] = [1, 0];
+          }
+        }
+        updateScore(scores);
         setWinner(squares[a]);
+      } else if (squares.find((element) => element === null) === undefined) {
+        scores[currentGame] = [0, 0];
+        updateScore(scores);
+        setDraw(true);
       }
     }
   };
@@ -81,13 +122,60 @@ const Board: FC<BoardProps> = () => {
     for (let index = 0; index < squares.length; index++) {
       squares[index] = null;
     }
+    setCurrentGame(currentGame + 1);
     changeX(true);
     setWinner(null);
+    setDraw(false);
+  };
+  const showScore = () => {
+    return scores.map((val, _, __) => {
+      return (
+        <Tr key={_}>
+          <Td textAlign="center">{val[0]}</Td>
+          <Td textAlign="center">{val[1]}</Td>
+        </Tr>
+      );
+    });
+  };
+  const showTable = () => {
+    return (
+      <Table mt={10} variant="simple">
+        <Thead>
+          <Tr>
+            <Th textAlign="center">
+              Player 1 ({currentGame % 2 === 0 ? "X" : "O"})
+            </Th>
+            <Th textAlign="center">
+              Player 2 ({currentGame % 2 === 1 ? "X" : "O"})
+            </Th>
+          </Tr>
+        </Thead>
+        <Tbody>{showScore()}</Tbody>
+      </Table>
+    );
+  };
+  const nextGame = () => {
+    for (let index = 0; index < squares.length; index++) {
+      squares[index] = null;
+    }
+    if (currentGame < numberOfGames - 1) {
+      setCurrentGame(currentGame + 1);
+      changeX(true);
+      setWinner(null);
+      setDraw(false);
+    }
   };
   return (
     <Box>
       <Text mb={10} textAlign="center">
-        The Current Player is {isX ? "X" : "O"}
+        The Current Player is{" "}
+        {isX
+          ? currentGame % 2 === 0
+            ? "Player 1 (X)"
+            : "Player 2 (X)"
+          : currentGame % 2 === 0
+          ? "Player 2 (O)"
+          : "Player 1 (O)"}
       </Text>
       <Flex>
         {renderSquare(0)}
@@ -109,25 +197,50 @@ const Board: FC<BoardProps> = () => {
           The winner is {winner}
         </Text>
       ) : null}
+      {isDraw ? (
+        <Text textAlign="center" mt={10}>
+          It's a Draw
+        </Text>
+      ) : null}
+      {isDraw || winner !== null ? (
+        <Center mt={5}>
+          <Button onClick={nextGame}>Go to the Next Game</Button>
+        </Center>
+      ) : null}
       <Center>
-        <Button onClick={resetBoard} mt={10}>
+        <Button
+          onClick={() => {
+            resetBoard();
+            setWinner(null);
+            setCurrentGame(0);
+            updateScore(Array<GameScore[]>(numberOfGames).fill(["-", "-"]));
+          }}
+          mt={10}
+        >
           Reset Game
         </Button>
       </Center>
+      {!isSingle ? showTable() : null}
     </Box>
   );
 };
 
 const Game: NextPage<RouterProps> = ({ router }) => {
-  console.log(router.query);
   return (
     <VStack>
       <Heading size="md" mt={10}>
         Tic Tac Toe Game
       </Heading>
-      <Board mode={router.query.mode} />
+      <Board mode={router.query.mode as GameMode} />
     </VStack>
   );
 };
+
+// This fixes a bug in NextJS where the query dissappears for a while
+export async function getServerSideProps() {
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+}
 
 export default withRouter(Game);
